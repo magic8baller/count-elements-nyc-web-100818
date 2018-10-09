0291b43407a0dbc5a57effb0981e82e7a0cf1ef5
github username <github email address>
2018-10-08 23:10:28 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,18 @@
 def count_elements(array)
+  new_hash = {}
+  array.each do |elem|
+    new_hash.has_key?(elem) ? new_hash[elem] += 1 : new_hash[elem] = 1
+  end
+  new_hash
   # code goes here
 end
- 
+
+#long:
+#if new_hash.has_key?(element)
+#  new_hash[element] += 1
+#else
+#  new_hash[element] = 1
+#end
+
+p animals = ['tortoise', 'aye-aye', 'honey badger', 'aye-aye', 'tortoise', 'tortoise']
+count_elements(animals)
